790ef9f561ec77bc0e4ea4efcef35434f1bb622c
togrul-asgerli <81424132+togrul-asgerli@users.noreply.github.com>
2022-10-08 01:29:13 +0400

    Add files via upload

---
+++ MassIndex/MassIndex/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MassIndex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_heigth = new System.Windows.Forms.TextBox();
+            this.textBox_weight = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Weight";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(79, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Height";
+            // 
+            // textBox_heigth
+            // 
+            this.textBox_heigth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_heigth.Location = new System.Drawing.Point(291, 157);
+            this.textBox_heigth.Multiline = true;
+            this.textBox_heigth.Name = "textBox_heigth";
+            this.textBox_heigth.Size = new System.Drawing.Size(147, 49);
+            this.textBox_heigth.TabIndex = 2;
+            // 
+            // textBox_weight
+            // 
+            this.textBox_weight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_weight.Location = new System.Drawing.Point(291, 82);
+            this.textBox_weight.Multiline = true;
+            this.textBox_weight.Name = "textBox_weight";
+            this.textBox_weight.Size = new System.Drawing.Size(147, 51);
+            this.textBox_weight.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(569, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Result.Location = new System.Drawing.Point(325, 307);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(226, 103);
+            this.textBox_Result.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MassIndex.Properties.Resources.bmi_body_mass_index_and_weight_loss_banner_background_2GGJTW9;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_weight);
+            this.Controls.Add(this.textBox_heigth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_heigth;
+        private System.Windows.Forms.TextBox textBox_weight;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_Result;
+    }
+}
+
---
+++ MassIndex/MassIndex/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MassIndex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double mass = Double.Parse(textBox_weight.Text);
+                double length = Double.Parse(textBox_heigth.Text);
+
+                double result = mass / (length * length);
+                if(mass==0 || length==0||mass<0 ||length<0)
+                {
+                    MessageBox.Show($"Inputs can't be {0} or less than {0}");
+                    return;
+                }
+                if(length<0 ||length>2.5)
+                {
+                    MessageBox.Show("Height has to be between 0 and 1");
+                    return;
+                }
+                if (result < 18.5)
+                {
+                    textBox_Result.Text = $"Your bmi is {result} and you are underweight";
+                }
+                else if (result >= 18.5 && result < 24.9)
+                {
+                    textBox_Result.Text = $"Your bmi is {result} and you are normal weight";
+                }
+                else if (result >= 24.9 && result < 29.9)
+                {
+                    textBox_Result.Text = $"Your bmi is {result} and you are overweight";
+                }
+                else if (result >= 29.9)
+                {
+                    textBox_Result.Text = $"Your bmi is {result} and you are obesity";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Enter valid data type!!");
+            }
+        }  
+    }
+}
